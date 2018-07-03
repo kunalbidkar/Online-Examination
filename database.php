bcbbf32588e77d6cb9a10f9d73e3d09913e42fe8
Kunal Bidkar <kunalbidkar13@gmail.com>
2018-07-03 19:23:43 -0400

    Update database.php

--- database.php
+++ database.php
@@ -1,4 +1,4 @@
 <?php
-$cn=mysql_connect("mysql.hostinger.co.uk","u476223707_kb","kb#kunal#") or die("Could not Connect My Sql");
+$cn=mysql_connect("mysql.hostinger.co.uk","u476223707_kb","") or die("Could not Connect My Sql");
 mysql_select_db("u476223707_kb",$cn)  or die("Could connect to Database");
 ?>
